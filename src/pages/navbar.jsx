0372f737250250abd3c78dce2457e8e75d2f2559
mavridiz <mavridis.gutierrez.sebastian@gmail.com>
2023-06-02 08:56:29 -0600

    v2.91

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                       >
-                        <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform w-screen max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                        <Popover.Panel className="absolute z-26 -ml-4 mt-3 transform w-screen max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
                           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                             <div className="relative grid gap-6 bg-zinc-900 px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
 
